40766ed8cfdf139381efe78f4381a933dc6b21d1
bala-amypo <bala@amypo.in>
2025-12-27 05:24:56 +0000

    User commit

--- src/main/java/com/example/demo/DemoServlet.java
+++ src/main/java/com/example/demo/DemoServlet.java
@@ -1,16 +1,20 @@
-package com.example.util;
+package com.example.demo;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 public class DemoServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
-            throws ServletException, IOException {
-        resp.getWriter().write("Hello from DemoServlet!");
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+
+        response.getWriter().write("DemoServlet is alive");
     }
 }
